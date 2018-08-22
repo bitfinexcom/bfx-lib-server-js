c658bc63e08274b13cea0ba808802ec7e2547ccb
face <face@myutil.com>
2018-08-21 21:06:03 -1000

    bug fixes

--- src/express/grenacheClientService.js
+++ src/express/grenacheClientService.js
@@ -2,6 +2,9 @@ const Grenache = require('grenache-nodejs-http')
 const Link = require('grenache-nodejs-link')
 const _ = require('lodash')
 const sanitaze = require('./validate')
+const { parseTokenIp, getIp } = require('./helpers')
+
+const Peer = Grenache.PeerRPCClient
 
 let peer = null
 const grenacheInit = (conf) => {
@@ -14,10 +17,10 @@ const grenacheInit = (conf) => {
   peer.init()
 }
 
-function request(query, res) {
+function request(service, query, res) {
   query = sanitaze(query)
   const timeout = _timeout(query.action)
-  peer.request('rest:core:kyc', query, timeout, (err, data) => ((err)
+  peer.request(service, query, timeout, (err, data) => ((err)
     ? res.json({ success: false, message: err.toString() })
     : res.json({ success: true, data })))
 }
@@ -27,15 +30,39 @@ function _timeout(action) {
   return { timeout }
 }
 
-function setRequest(action, extra) {
+function setGrenacheRequest(service, action, extra) {
   return (req, res) => {
     const add = (extra)
       ? extra(req)
       : {}
     const args = [_.assign({}, req.query, req.body, add)]
     const query = { action, args }
-    request(query, res)
+    request(service, query, res)
   }
 }
 
-module.exports = { setRequest }
+// Types of requests
+function getGrenacheReqWithAuth(service, action, collection) {
+  const setExtra = (req) => {
+    const auth = parseTokenIp(req)
+    return (collection)
+      ? { auth, collection }
+      : { auth }
+  }
+  return setGrenacheRequest(service, action, setExtra)
+}
+
+function getGrenacheReqWithIp(service, action) {
+  const setIp = (req) => {
+    const ip = getIp(req)
+    return { ip }
+  }
+  return setGrenacheRequest(service, action, setIp)
+}
+
+module.exports = {
+  grenacheInit,
+  setGrenacheRequest,
+  getGrenacheReqWithAuth,
+  getGrenacheReqWithIp,
+}
