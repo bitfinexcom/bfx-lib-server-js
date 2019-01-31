b189adaf80f910d0ffeb4f22f3aa502f7b8216df
face <face@myutil.com>
2019-01-31 04:41:18 -1000

    support for efx-token-sales-ui sytle auth

--- src/express/helpers.js
+++ src/express/helpers.js
@@ -2,8 +2,20 @@ const requestIp = require('request-ip')
 
 function parseTokenIp (req) {
   const ip = requestIp.getClientIp(req)
-  const { token } = req.headers
-  return [token, { ip }]
+  const { token, auth } = req.headers
+  if (token) {
+    return [token, { ip }]
+  }
+  let authObject = {}
+  try {
+    if (auth) {
+      authObject = JSON.parse(auth)
+    }
+  } catch (e) {
+    console.error(e)
+  }
+  authObject.ip = ip
+  return authObject
 }
 
 function getIp (req) {
