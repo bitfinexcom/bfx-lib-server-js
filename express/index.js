bf94e2af8c42e69fdf33b95926d1510a74695b27
ezeswci <wernicke.ezequiel@gmail.com>
2019-12-11 16:20:25 +1100

    Move express folder
    Move express folder outside src as src is something from UI and express 
    is back end.

---
+++ express/index.js
@@ -0,0 +1,13 @@
+'use strict'
+
+import config from './config'
+import grenacheClientService from './grenacheClientService'
+import helpers from './helpers'
+import validate from './validate'
+
+export default {
+  config,
+  grenacheClientService,
+  helpers,
+  validate
+}
---
+++ express/validate.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const sanitizer = require('sanitizer')
+const _ = require('lodash')
+
+function sanitaze (query) {
+  return _sanitazer(query)
+}
+
+function _sanitazer (val) {
+  if (_.isArray(val)) return _sanitazeArr(val)
+  if (_.isPlainObject(val)) return _sanitazeObj(val)
+  return (typeof val === 'string') ? sanitizer.sanitize(val) : val
+}
+
+function _sanitazeArr (arr) {
+  return _.map(arr, _sanitazer)
+}
+
+function _sanitazeObj (obj) {
+  return _.reduce(obj, (result, value, key) => {
+    result[_sanitazer(key)] = _sanitazer(value)
+    return result
+  }, {})
+}
+
+module.exports = sanitaze
